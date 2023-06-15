bdf11a14b97bd8eb805b5574d8aefc71d745cebb
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2023-06-15 21:23:30 +0530

    feat: #4 profile update api added

--- custom/kasi_api/controllers/business.py
+++ custom/kasi_api/controllers/business.py
@@ -1,6 +1,7 @@
 from odoo.http import route, request, Controller
 from .services import validate_request
 from odoo.addons.kasi_api.models.business import Business
+from odoo.addons.kasi_api.models.address import Address
 
 class BusinessController(Controller):
 
@@ -12,16 +13,31 @@ class BusinessController(Controller):
         response = {'status':200,'response':Business.Types,'message':"success"}
         return response
 
-    @route('/api/business/townships/',auth='public',type='json',methods=['POST','OPTIONS'],cors=cors)
-    def get_townships(self,**kwargs):
-        validate_request(kwargs)
-        response = {'status':200,'response':Business.Townships,'message':"success"}
-        return response
-
     @route('/api/business/<int:id>/',auth='user',type='json',methods=['POST','OPTIONS'],cors=cors)
     def get_buisness(self,id,**kwargs):
         validate_request(kwargs)
-        business = request.env['res.company'].sudo().search_read([('id','=',id)])
-        return business
+        business = request.env['res.company'].sudo().search([('id','=',id)])
+        partner = business.partner_id
+        business_dict = business.read()[0]
+        business_dict["address"] = self._get_partner_address(partner)
+        response = {'status':200,'response':business_dict,'message':"success"}
+        return response
+
+    def _get_partner_address(self, partner):
+        address = {
+            "street": partner.street,
+            "city": partner.city, 
+            "province": partner.province,
+            "zip": partner.zip,
+            "landmark": partner.landmark,
+            "township": partner.township 
+        }
+        return address
+
+    @route('/api/address/townships/',auth='public',type='json',methods=['POST','OPTIONS'],cors=cors)
+    def get_townships(self,**kwargs):
+        validate_request(kwargs)
+        response = {'status':200,'response':Address.Townships,'message':"success"}
+        return response
 
     
