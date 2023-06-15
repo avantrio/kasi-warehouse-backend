537dd8bcef19ea1724e7e503caa9e105babd4c69
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2023-06-15 14:17:21 +0530

    fix : #4 undoing unwanted commits

--- custom/kasi_api/__manifest__.py
+++ custom/kasi_api/__manifest__.py
@@ -1,13 +1,9 @@
 {
-    'name': 'Kasi Api',
+    'name': 'kasi-rest-api',
     'version': '1.0',
     'category': 'REST API',
     'sequence': 15,
     'summary': 'Kasi warehouse REST API',
     'description': "Kasi warehouse REST API",
-    'website': 'https://www.odoo.com/',
-    'data': [
-
-    ],
-    'installable': True
+    'website': 'https://www.odoo.com/'
 }
