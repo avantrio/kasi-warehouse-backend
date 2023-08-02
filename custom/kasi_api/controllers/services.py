e38c18243de52f23d239b1119a49ef4847cab4fb
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2023-08-02 14:04:38 +0530

    fix: fixed mobile number character issue

--- custom/kasi_api/controllers/services.py
+++ custom/kasi_api/controllers/services.py
@@ -6,13 +6,18 @@ import phonenumbers
 from phonenumbers import carrier
 from phonenumbers.phonenumberutil import number_type
 from math import ceil
+import re
 
 def validate_request(kwargs):
     if 'method' not in kwargs or kwargs.get('method') != 'GET':
         raise MethodNotAllowed()
     
 def verify_mobile_number(number):
-    return carrier._is_mobile(number_type(phonenumbers.parse(number)))
+    pattern = r"^\+?\d+$"
+    if bool(re.match(pattern, number)):
+        return carrier._is_mobile(number_type(phonenumbers.parse(number)))
+    else:
+        return False
         
 def paginate(page,page_size,total):
     result = {}
