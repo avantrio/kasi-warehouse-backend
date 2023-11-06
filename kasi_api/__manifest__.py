6e4160f3dfedd2144c194e00a37d03dadaa4181a
Raam124 <ramvijay124@gmail.com>
2023-11-06 15:09:35 +0530

    kasi plugin release 2.1.4

---
+++ kasi_api/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'kasi_rest_api',
+    'version': '2.1.4',
+    'category': 'REST API',
+    'sequence': 15,
+    'summary': 'Kasi warehouse REST API',
+    'description': "Kasi warehouse REST API",
+    'website': 'https://www.odoo.com/',
+    "depends": [
+        "sale",
+    ],
+    "data":[
+        'security/ir.model.access.csv',
+        'views/kasi_deals.xml',
+    ]
+}
