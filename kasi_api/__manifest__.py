30674eb61154f5e8779dfa2fcf94c50bec612389
Raam124 <ramvijay124@gmail.com>
2023-11-21 11:27:54 +0530

    kasi plugin release 2.1.7
    appropriate error messages and admin ui store name change

---
+++ kasi_api/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'kasi_rest_api',
+    'version': '2.1.7',
+    'category': 'REST API',
+    'sequence': 15,
+    'summary': 'Kasi warehouse REST API',
+    'description': "Kasi warehouse REST API",
+    'website': 'https://www.odoo.com/',
+    "depends": [
+        "sale",
+    ],
+    "data":[
+        'security/ir.model.access.csv',
+        'views/kasi_deals.xml',
+        'views/res_partner.xml',
+    ]
+}
